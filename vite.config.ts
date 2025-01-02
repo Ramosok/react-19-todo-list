1c257dbc011653966ffdc39bfd7a4f520e075c72
Vitaliy <ramosok86@gmail.com>
2025-01-02 12:30:21 +0300

    test 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: './',
+  base: '/react-19-todo-list',
   plugins: [react(), tsconfigPaths()],
 })
